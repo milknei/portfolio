ff24a534a6ae07cdf4d8722559e2fc84183cefba
Danial Klymenko <danilsmanagement@gmail.com>
2023-09-10 16:47:25 +0300

    About section

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
     icon: web,
   },
   {
-    title: 'React Native Developer',
+    title: 'NextJS-React Developer',
     icon: mobile,
   },
   {
-    title: 'Backend Developer',
+    title: 'Frontend Developer',
     icon: backend,
   },
   {
-    title: 'Content Creator',
+    title: 'Angular Developer',
     icon: creator,
   },
 ];
