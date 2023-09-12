1467b0638a5197c54abd26a16bdabce3849f60e2
Danial Klymenko <danilsmanagement@gmail.com>
2023-09-12 20:21:29 +0300

    Finished template

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           <img src={logoSimple} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Danylo Klymenko &nbsp;
-            <span className="sm:block hidden"> | Portfolio</span>
+            <span className="sm:block hidden"> |&nbsp;&nbsp;Portfolio</span>
           </p>
         </Link>
 
